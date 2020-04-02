abfe651d50ef0a4bf97bbf67eff09650bb9c8c47
Yaskadju <thales.y@gmail.com>
2020-04-02 15:15:31 -0300

    2 files modified

--- components/Home.js
+++ components/Home.js
@@ -2,50 +2,44 @@ import React, {Component} from 'react'
 import axios from 'axios'
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: []
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
     }
+  }
 
-    async componentDidMount() {
-        const resp = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        console.log(resp)
-        this.setState({
-            posts: resp.data.slice(0, 10)
-        })
-    }
+  async componentDidMount() {
+    const resp = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    
+    this.setState({
+      posts: resp.data.slice(0, 10)
+    })
+  }
 
-    render() {
-
-        const {posts} = this.state
-        const postList = posts.length ? (
-            posts.map(post => {
-                return (
-                    <div className="post card" key={post.id}>
-                        <div className="card-content">
-                            <span className="card-title">
-                                {post.title}
-                            </span>
-                            <p>{post.body}</p>
-                        </div>
-                    </div>
-                )
-            })
-        ) : (
-            <div className="center">
-                No posts yet
-            </div>
-        ) 
-
+  render() {
+    const postList = this.state.posts.length ? (
+      this.state.posts.map(post => {
         return (
-            <div className="container">
-                <h4 className="center">Home</h4>
-                {postList}
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
             </div>
+          </div>
         )
-    }
+      })
+    ) : (
+      <div className="center">There is no post left</div>
+    )
+
+    return (
+      <div>
+        {postList}
+      </div>
+    )
+  }
+    
 }
 
 export default Home
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Navbar from './components/Navbar'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './components/Home'
+import { render } from 'react-dom';
 import About from './components/About'
 import Contact from './components/Contact'
 
@@ -15,16 +16,18 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <div>
+        <BrowserRouter>
         <div className="App">
           <Navbar/>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
         </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </div>
     )
   }
 }
 
-export default App
+render(<App />, document.getElementById('root'));
