dc94c763a271454d21f8de71d32d859784e43de6
Yaskadju <thales.y@gmail.com>
2020-04-02 16:39:00 -0300

    1 binaries uploaded

--- components/Home.js
+++ components/Home.js
@@ -1,45 +1,54 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      posts: []
+    constructor(props) {
+        super(props)
+        this.state = {
+            posts: []
+        }
     }
-  }
 
-  async componentDidMount() {
-    const resp = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    
-    this.setState({
-      posts: resp.data.slice(0, 10)
-    })
-  }
+    async componentDidMount() {
+        const resp = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        
+        this.setState({
+            posts: resp.data.slice(0, 10)
+        })
+    }
 
-  render() {
-    const postList = this.state.posts.length ? (
-      this.state.posts.map(post => {
-        return (
-          <div className="post card" key={post.id}>
-            <div className="card-content">
-              <span className="card-title">{post.title}</span>
-              <p>{post.body}</p>
+    render() {
+
+        const {posts} = this.state
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <img src={Pokeball} alt="a pokeball"/>
+                        <div className="card-content">
+                            <Link to={'/' + post.id}>
+                                <span className="card-title red-text"> {post.title} </span>
+                            </Link>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">
+                No posts yet
             </div>
-          </div>
-        )
-      })
-    ) : (
-      <div className="center">There is no post left</div>
-    )
+        ) 
 
-    return (
-      <div>
-        {postList}
-      </div>
-    )
-  }
-    
+        return (
+            <div className="container home">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home
